7fc4e53bb1370ef95f3c8eedfca6b24307d1800c
Alejandro Capellan <acapellan@capw.com.do>
2023-01-11 23:25:54 -0400

    Update __manifest__.py

--- l10n_do_currency_update/__manifest__.py
+++ l10n_do_currency_update/__manifest__.py
@@ -6,11 +6,7 @@
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
-<<<<<<< HEAD
-    "version": "14.0.2.1.0",
-=======
     "version": "15.0.1.0.0",
->>>>>>> 7c4183ce94ed163916759ae4253eed777d61441b
     "depends": ["account"],
     "data": [
         "data/ir_cron_data.xml",
