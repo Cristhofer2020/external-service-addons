78185e9ba21c59918e7122f0a2fb2c2d199cfd44
Alejandro Capellan <acapellan@capw.com.do>
2023-01-11 23:27:01 -0400

    Update __manifest__.py

--- l10n_do_ncf_validation/__manifest__.py
+++ l10n_do_ncf_validation/__manifest__.py
@@ -1,10 +1,6 @@
 {
     "name": "Dominican NCF Validation",
-<<<<<<< HEAD
-    "version": "14.0.1.0.0",
-=======
     "version": "15.0.1.0.0",
->>>>>>> 7c4183ce94ed163916759ae4253eed777d61441b
     "summary": "Validate NCF from external service",
     "category": "Extra Tools",
     "author": "Indexa",
@@ -14,9 +10,5 @@
         "data/ir_config_parameter_data.xml",
         "views/res_config_settings_views.xml",
     ],
-<<<<<<< HEAD
-    "installable": False,
-=======
     "installable": True,
->>>>>>> 7c4183ce94ed163916759ae4253eed777d61441b
 }
